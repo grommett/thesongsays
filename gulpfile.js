1d74cf6c97fac69a0e2302b57ebffbd3392cf2a5
David Ford <david@smithandrobot.com>
2014-05-31 14:34:22 -0500

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require("gulp");
 var deploy = require("gulp-gh-pages");
 
 gulp.task('deploy', function () {
-    gulp.src("./_site/**/*")
-        .pipe(deploy({push:false}));
+    gulp.src("./dist/**/*")
+        .pipe(deploy({push:true}));
 });
