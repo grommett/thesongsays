45fd9e6a0c9feeb373127e56b32b52f7d0e3a416
David <david@pinkiering.com>
2016-04-19 21:24:33 -0500

    adding press for b+s, bruno and public lover

--- src/data/press.js
+++ src/data/press.js
@@ -31,5 +31,132 @@ module.exports = [
         link: "https://www.xlr8r.com/mp3/2016/03/visuals-everything-flash/"
       }
     ]
+  },
+  {
+    artist: 'Benoit & Sergio',
+    press: [
+      {
+        label: "Infusion Magazine Talks To Benoit & Sergio",
+        link: "http://www.infusion.ae/en/article/magazine/features/benoit-sergio-interview.html"
+      },
+      {
+        label: "Pete Tong Interviews Benoit & Sergio",
+        link: "https://www.youtube.com/watch?v=VDXmBhHVOz8"
+      },
+      {
+        label: "Deep House Amsterdam Talk To Benoit & Sergio",
+        link: "http://www.deephouseamsterdam.com/exclusive-interview-benoit-sergio/"
+      },
+      {
+        label: "Exchange On Resident Advisor",
+        link: "https://www.residentadvisor.net/podcast-episode.aspx?exchange=168"
+      },
+      {
+        label: "Taking The Talk With Dazed Digital",
+        link: "http://www.dazeddigital.com/music/article/10837/1/benoit-sergio-talking-the-talk"
+      },
+      {
+        label: "What I've Lost On Resident Advisor",
+        link: "http://www.residentadvisor.net/review-view.aspx?id=6872"
+      }
+    ]
+  },
+  {
+    artist: 'Bruno Pronsato',
+    press: [
+      {
+        label: "Lovers Do On The Gap",
+        link: "http://www.thegap.at/reviews/review/bruno-pronsato/lovers-do/"
+      },
+      {
+        label: "Bruno Pronsato's Lovers Do On Resident Advisor",
+        link: "http://www.residentadvisor.net/news.aspx?id=32926"
+      },
+      {
+        label: "Lovers Do On Sigmund Fraud",
+        link: "http://sigmundfraud.us/review-bruno-pronsato-lovers-do/"
+      },
+      {
+        label: "Bruno Pronsato Talks to Blisspop",
+        link: "http://www.blisspop.com/blog/2011/09/15/interview-with-bruno-pronsato/"
+      },
+      {
+        label: "Lovers Do On Pitchfork",
+        link: "http://pitchfork.com/reviews/albums/15550-lovers-do/"
+      },
+      {
+        label: "Lovers Do On Exclaim.ca",
+        link: "http://exclaim.ca/music/article/bruno_pronsato-lovers_do"
+      },
+      {
+        label: "Lovers Do On Input Selector",
+        link: "http://www.inputselector.fr/2011/05/19/bruno-pronsato-lovers-do-thesongsays/"
+      },
+      {
+        label: "Electro : le trublion Bruno Pronsato sort « Lovers Do »",
+        link: "https://cm2c.wordpress.com/2011/03/31/electro-bruno-pronsato-sort-son-lovers-do/"
+      },
+      {
+        label: "Bruno Pronsato Says Lovers Do",
+        link: "http://www.residentadvisor.net/news.aspx?id=13718"
+      },
+      {
+        label: "Anybody But You On Resident Advisor",
+        link: "http://www.residentadvisor.net/review-view.aspx?id=8633"
+      },
+      {
+        label: "The Make Up The Break Up On Resident Advisor",
+        link: "http://www.residentadvisor.net/review-view.aspx?id=6483"
+      },
+      {
+        label: "Bruno Pronsato Debuts New Label",
+        link: "http://www.residentadvisor.net/news.aspx?id=10354"
+      }
+    ]
+  },
+  {
+    artist: 'Public Lover',
+    press: [
+      {
+        label: "A Broken Shape Of You On Longueur d'Ondes",
+        link: "http://www.longueurdondes.com/2011/12/22/public-lover/"
+      },
+      {
+        label: "A Broken Shape Of You On Groove.de",
+        link: "http://www.groove.de/2012/02/21/public-lover-a-broken-shape-of-you-album-review/"
+      },
+      {
+        label: "A Broken Shape Of You On Ibiza Voice",
+        link: "http://www.ibiza-voice.com/story/news/3785"
+      },
+      {
+        label: "Public Lover Unveil Debut Album, A Broken Shape of You",
+        link: "http://www.residentadvisor.net/news.aspx?id=15056"
+      },
+      {
+        label: "Public Lover Interview for Ibiza Voice+ Exclusive Live Mix",
+        link: "http://www.ibiza-voice.com/story/news/3014"
+      },
+      {
+        label: "Public Lover Exclusive Live Mix for Ibiza Voice",
+        link: "http://www.ibiza-voice.com/story/news/3014"
+      },
+      {
+        label: "Musique D'Hiver Pour L'Ete On Resident Advisor",
+        link: "http://www.residentadvisor.net/review-view.aspx?id=8163"
+      },
+      {
+        label: "Feed Me Rainbows On De:Bug",
+        link: "http://de-bug.de/reviews/nina-leece-feed-me-rainbows-thesongsays-003/"
+      },
+      {
+        label: "Feed Me Rainbows On Resident Advisor",
+        link: "http://www.residentadvisor.net/review-view.aspx?id=7436"
+      },
+      {
+        label: "Feed Me Rainbows On Little White Earbuds",
+        link: "http://www.littlewhiteearbuds.com/review/ninca-leece-feed-me-rainbows/#.VsMUGIfVv8s"
+      }
+    ]
   }
 ]
